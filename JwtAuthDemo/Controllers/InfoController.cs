a7215d202feb4ba09f04672af9d3b0608d89a5ea
ming.yu <ming.yu@molife.com.tw>
2021-11-26 16:11:30 +0800

    init

---
+++ JwtAuthDemo/Controllers/InfoController.cs
@@ -0,0 +1,40 @@
+﻿using JwtAuthDemo.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtAuthDemo.Controllers
+{
+    [Authorize]
+    public class InfoController : Controller
+    {
+        private readonly JwtHelpers jwt;
+
+        public InfoController(JwtHelpers jwt)
+        {
+            this.jwt = jwt;
+        }
+
+        [HttpGet("~/claims")]
+        public IActionResult GetClaims()
+        {
+            return Ok(User.Claims.Select(p => new { p.Type, p.Value }));
+        }
+
+        [HttpGet("~/username")]
+        public IActionResult GetUserName()
+        {
+            return Ok(User.Identity.Name);
+        }
+
+        [HttpGet("~/jwtid")]
+        public IActionResult GetUniqueId()
+        {
+            var jti = User.Claims.FirstOrDefault(p => p.Type == "jti");
+            return Ok(jti.Value);
+        }
+    }
+}
---
+++ JwtAuthDemo/Controllers/TokenController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JwtAuthDemo.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using JwtAuthDemo.Models;
+
+namespace JwtAuthDemo.Controllers
+{
+    [Authorize]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly JwtHelpers jwt;
+
+        public TokenController(JwtHelpers jwt)
+        {
+            this.jwt = jwt;
+        }
+
+        [AllowAnonymous]
+        [Route("SignIn/{aa}")]
+        [HttpPost]
+        public ActionResult<string> SignIn(LoginViewModel login,int? aa )
+        {
+
+            if (ValidateUser(login))
+            {
+                return jwt.GenerateToken(login.Username);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        private bool ValidateUser(LoginViewModel login)
+        {
+            return true; // TODO
+        }
+
+    }
+}
